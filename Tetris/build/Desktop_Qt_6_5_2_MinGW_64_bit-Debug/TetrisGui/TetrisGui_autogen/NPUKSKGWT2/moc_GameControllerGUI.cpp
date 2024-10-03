83493644ae5d6b1becd5b9fcbde7cc296039381e
zkr1070 <60423@etu.he2b.be>
2024-10-04 00:28:25 +0200

    Tetris added

---
+++ Tetris/build/Desktop_Qt_6_5_2_MinGW_64_bit-Debug/TetrisGui/TetrisGui_autogen/NPUKSKGWT2/moc_GameControllerGUI.cpp
@@ -0,0 +1,224 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'GameControllerGUI.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "C:/Users/zakmh/OneDrive/Bureau/ESI-INFO/2eme/Dev4/Projet/depots/dev4_nasri_mhamdi/Application/Tetris/TetrisGui/controller/GameControllerGUI.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'GameControllerGUI.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSGameControllerGUIENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSGameControllerGUIENDCLASS = QtMocHelpers::stringData(
+    "GameControllerGUI",
+    "moveLeft",
+    "",
+    "moveRight",
+    "moveDown",
+    "rotateClockwise",
+    "rotateCounterClockwise",
+    "drop",
+    "nextBrick",
+    "pauseAndStartTimer",
+    "handleKeyPress",
+    "key"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSGameControllerGUIENDCLASS_t {
+    uint offsetsAndSizes[24];
+    char stringdata0[18];
+    char stringdata1[9];
+    char stringdata2[1];
+    char stringdata3[10];
+    char stringdata4[9];
+    char stringdata5[16];
+    char stringdata6[23];
+    char stringdata7[5];
+    char stringdata8[10];
+    char stringdata9[19];
+    char stringdata10[15];
+    char stringdata11[4];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSGameControllerGUIENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSGameControllerGUIENDCLASS_t qt_meta_stringdata_CLASSGameControllerGUIENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 17),  // "GameControllerGUI"
+        QT_MOC_LITERAL(18, 8),  // "moveLeft"
+        QT_MOC_LITERAL(27, 0),  // ""
+        QT_MOC_LITERAL(28, 9),  // "moveRight"
+        QT_MOC_LITERAL(38, 8),  // "moveDown"
+        QT_MOC_LITERAL(47, 15),  // "rotateClockwise"
+        QT_MOC_LITERAL(63, 22),  // "rotateCounterClockwise"
+        QT_MOC_LITERAL(86, 4),  // "drop"
+        QT_MOC_LITERAL(91, 9),  // "nextBrick"
+        QT_MOC_LITERAL(101, 18),  // "pauseAndStartTimer"
+        QT_MOC_LITERAL(120, 14),  // "handleKeyPress"
+        QT_MOC_LITERAL(135, 3)   // "key"
+    },
+    "GameControllerGUI",
+    "moveLeft",
+    "",
+    "moveRight",
+    "moveDown",
+    "rotateClockwise",
+    "rotateCounterClockwise",
+    "drop",
+    "nextBrick",
+    "pauseAndStartTimer",
+    "handleKeyPress",
+    "key"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSGameControllerGUIENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   68,    2, 0x08,    1 /* Private */,
+       3,    0,   69,    2, 0x08,    2 /* Private */,
+       4,    0,   70,    2, 0x08,    3 /* Private */,
+       5,    0,   71,    2, 0x08,    4 /* Private */,
+       6,    0,   72,    2, 0x08,    5 /* Private */,
+       7,    0,   73,    2, 0x08,    6 /* Private */,
+       8,    0,   74,    2, 0x08,    7 /* Private */,
+       9,    0,   75,    2, 0x08,    8 /* Private */,
+      10,    1,   76,    2, 0x08,    9 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   11,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject GameControllerGUI::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_CLASSGameControllerGUIENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSGameControllerGUIENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSGameControllerGUIENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<GameControllerGUI, std::true_type>,
+        // method 'moveLeft'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'moveRight'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'moveDown'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'rotateClockwise'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'rotateCounterClockwise'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'drop'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'nextBrick'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pauseAndStartTimer'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'handleKeyPress'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>
+    >,
+    nullptr
+} };
+
+void GameControllerGUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<GameControllerGUI *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->moveLeft(); break;
+        case 1: _t->moveRight(); break;
+        case 2: _t->moveDown(); break;
+        case 3: _t->rotateClockwise(); break;
+        case 4: _t->rotateCounterClockwise(); break;
+        case 5: _t->drop(); break;
+        case 6: _t->nextBrick(); break;
+        case 7: _t->pauseAndStartTimer(); break;
+        case 8: _t->handleKeyPress((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *GameControllerGUI::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GameControllerGUI::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSGameControllerGUIENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int GameControllerGUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 9;
+    }
+    return _id;
+}
+QT_WARNING_POP
